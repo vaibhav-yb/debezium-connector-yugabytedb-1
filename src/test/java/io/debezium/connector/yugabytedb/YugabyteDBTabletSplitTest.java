2f1403d9973e269d6d3da2d4533cb9c8ee4c6f6d
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-11-09 09:41:47 +0000

    tablet split tests

--- src/test/java/io/debezium/connector/yugabytedb/YugabyteDBTabletSplitTest.java
+++ src/test/java/io/debezium/connector/yugabytedb/YugabyteDBTabletSplitTest.java
@@ -35,12 +35,13 @@ public class YugabyteDBTabletSplitTest extends YugabyteDBTestBase {
 
   @BeforeClass
   public static void beforeClass() throws SQLException {
-      ybContainer = TestHelper.getYbContainer();
-      ybContainer.start();
+      // ybContainer = TestHelper.getYbContainer();
+      // ybContainer.start();
 
-      TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
-      TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
-      masterAddresses = ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100);
+      // TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
+      // TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
+      // masterAddresses = ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100);
+      masterAddresses = "127.0.0.1:7100";
 
       TestHelper.dropAllSchemas();
   }
@@ -58,7 +59,7 @@ public class YugabyteDBTabletSplitTest extends YugabyteDBTestBase {
 
   @AfterClass
   public static void afterClass() throws Exception {
-      ybContainer.stop();
+      // ybContainer.stop();
   }
 
   @Test
@@ -107,12 +108,7 @@ public class YugabyteDBTabletSplitTest extends YugabyteDBTestBase {
     ybClient.splitTablet(tablets.iterator().next());
 
     // Wait till there are 2 tablets for the table.
-    Awaitility.await()
-      .pollDelay(Duration.ofSeconds(2))
-      .atMost(Duration.ofSeconds(20))
-      .until(() -> {
-        return ybClient.getTabletUUIDs(table).size() == 2;
-      });
+    waitForTablets(ybClient, table, 2);
 
     // Insert more records
     for (int i = recordsCount; i < 100; ++i) {
@@ -131,5 +127,103 @@ public class YugabyteDBTabletSplitTest extends YugabyteDBTestBase {
       ybClient.getTabletListToPollForCdc(table, dbStreamId, table.getTableId());
 
     assertEquals(2, getTabletResponse2.getTabletCheckpointPairListSize());
+  }
+
+  @Test
+  public void shouldConsumeDataAfterMultipleTabletSplits() throws Exception {
+    TestHelper.dropAllSchemas();
+    TestHelper.execute("CREATE TABLE t1 (id INT PRIMARY KEY, name TEXT) SPLIT INTO 3 TABLETS;");
+
+    String dbStreamId = TestHelper.getNewDbStreamId("yugabyte", "t1");
+    Configuration.Builder configBuilder = TestHelper.getConfigBuilder("public.t1", dbStreamId);
+
+    start(YugabyteDBConnector.class, configBuilder.build(), (success, message, error) -> {
+      assertTrue(success);
+    });
+
+    awaitUntilConnectorIsReady();
+
+    int recordsCount = 50;
+
+    String insertFormat = "INSERT INTO t1 VALUES (%d, 'value for split table');";
+
+    for (int i = 0; i < recordsCount; ++i) {
+      TestHelper.execute(String.format(insertFormat, i));
+    }
+
+    YBClient ybClient = TestHelper.getYbClient(masterAddresses);
+    YBTable table = TestHelper.getYbTable(ybClient, "t1");
+    
+    // Verify that there is just a single tablet.
+    Set<String> tablets = ybClient.getTabletUUIDs(table);
+    int tabletCountBeforeSplit = tablets.size();
+    assertEquals(3, tabletCountBeforeSplit);
+
+    // Also verify that the new API to get the tablets is returning the correct tablets.
+    GetTabletListToPollForCDCResponse getTabletsResponse =
+      ybClient.getTabletListToPollForCdc(table, dbStreamId, table.getTableId());
+    assertEquals(tabletCountBeforeSplit, getTabletsResponse.getTabletCheckpointPairListSize());
+
+    // Compact the table to ready it for splitting.
+    ybClient.flushTable(table.getTableId());
+
+    // Wait for 20s for the table to be flushed.
+    TestHelper.waitFor(Duration.ofSeconds(20));
+
+    // Split 1 tablet.
+    ybClient.splitTablet(tablets.iterator().next());
+
+    // Wait till there are 4 tablets for the table (2 existing plus one tablet split into 2).
+    waitForTablets(ybClient, table, 4);
+
+    // Insert more records now --> total 1500 records here.
+    TestHelper.executeBulkWithRange(insertFormat, recordsCount, 1500);
+
+    // Wait for the parent tablet to be deleted before splitting again.
+    TestHelper.waitFor(Duration.ofSeconds(20));
+
+    // Flush the table and split all the tablets.
+    ybClient.flushTable(table.getTableId());
+
+    // Wait for 20s for the table to be flushed.
+    TestHelper.waitFor(Duration.ofSeconds(20));
+
+    // Split each of the new tablets.
+    Set<String> tabletSet = ybClient.getTabletUUIDs(table);
+
+    // Verify that there are 4 tablets in total.
+    assertEquals(4, tabletSet.size());
+
+    for (String tabletToSplit : tabletSet) {
+      ybClient.splitTablet(tabletToSplit);
+    }
+
+    // Wait for the tablet count to reach 8 since we would have split all the existing 4 tablets.
+    waitForTablets(ybClient, table, 8);
+
+    // Consume the records now - there will be 1500 records in total.
+    SourceRecords records = consumeRecordsByTopic(1500);
+
+    // Verify that the records are there in the topic.
+    assertEquals(1500, records.recordsForTopic("test_server.public.t1").size());
+
+    // Verify that the API to read cdc_state is returning the correct set of tablets.
+    GetTabletListToPollForCDCResponse responseAfterSecondSplit = ybClient.getTabletListToPollForCdc(
+        table, dbStreamId, table.getTableId());
+    assertEquals(8, responseAfterSecondSplit.getTabletCheckpointPairListSize());
+  }
+
+  @Test
+  public void shouldConsumeDataAfterMultiLevelSplitting() throws Exception {
+    
+  }
+
+  private void waitForTablets(YBClient ybClient, YBTable table, int tabletCount) {
+    Awaitility.await()
+      .pollDelay(Duration.ofSeconds(2))
+      .atMost(Duration.ofSeconds(20))
+      .until(() -> {
+        return ybClient.getTabletUUIDs(table).size() == tabletCount;
+      });
   }
 }
