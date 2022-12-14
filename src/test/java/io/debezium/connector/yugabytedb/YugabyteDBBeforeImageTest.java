0c5ee67bcb488f15ea60dea703add26e47112f14
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-12-14 09:08:06 +0000

    working test for repro - repro not successful yet

--- src/test/java/io/debezium/connector/yugabytedb/YugabyteDBBeforeImageTest.java
+++ src/test/java/io/debezium/connector/yugabytedb/YugabyteDBBeforeImageTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,25 +25,27 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.YugabyteYSQLContainer;
+import org.yb.client.YBClient;
 
 import io.debezium.config.Configuration;
 import io.debezium.connector.yugabytedb.common.YugabyteDBTestBase;
+import io.debezium.connector.yugabytedb.connection.YugabyteDBConnection;
 
 public class YugabyteDBBeforeImageTest extends YugabyteDBTestBase {
   private final static Logger LOGGER = LoggerFactory.getLogger(YugabyteDBPartitionTest.class);
   private final String formatInsertString =
       "INSERT INTO t1 VALUES (%d, 'Vaibhav', 'Kushwaha', 12.345);";
   private static YugabyteYSQLContainer ybContainer;
-  private static String masterAddresses;
+//   private static String masterAddresses;
 
   @BeforeClass
   public static void beforeClass() throws SQLException {
-      ybContainer = TestHelper.getYbContainer();
-      ybContainer.start();
+    //   ybContainer = TestHelper.getYbContainer();
+    //   ybContainer.start();
 
-      TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
-      TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
-      masterAddresses = ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100);
+    //   TestHelper.setContainerHostPort(ybContainer.getHost(), ybContainer.getMappedPort(5433));
+    //   TestHelper.setMasterAddress(ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100));
+    //   masterAddresses = ybContainer.getHost() + ":" + ybContainer.getMappedPort(7100);
 
       TestHelper.dropAllSchemas();
   }
@@ -59,7 +63,7 @@ public class YugabyteDBBeforeImageTest extends YugabyteDBTestBase {
 
   @AfterClass
   public static void afterClass() throws Exception {
-      ybContainer.stop();
+    //   ybContainer.stop();
   }
 
   @Test
@@ -300,6 +304,170 @@ public class YugabyteDBBeforeImageTest extends YugabyteDBTestBase {
     assertAfterImage(record2, 1, "updated_first_name_2", "updated_last_name", 98.765);
   }
 
+  @Test
+  public void shouldHandleDeletesGracefullyWithoutErrors() throws Exception {
+    // TestHelper.initDB("yugabyte_create_tables.ddl");
+    int recordsCount = 20000;
+    int batchSize = 1000;
+    int iterations = recordsCount / batchSize;
+    int columnCount = 99;
+    long millisecondsToWait = 600 * 1000; // Wait this much for the records to be consumed
+
+    TestHelper.execute("DROP TABLE IF EXISTS t1;");
+
+    String generatedColumns = "";
+    
+    for (int i = 1; i <= columnCount; ++i) {
+      generatedColumns += "v" + i
+                          + " varchar(400) default "
+                          + "'123456789012345678901234567890123456789012345678901234567890"
+                          + "123456789012345678901234567890123456789012345678901234567890"
+                          + "123456789012345678901234567890123456789012345678901234567890"
+                          + "123456789012345678901234567890123456789012345678901234567890"
+                          + "1234567890123456789012345678901234567890123456789012345', ";
+    }
+
+    TestHelper.execute("CREATE TABLE t1 (id INT PRIMARY KEY, name TEXT, "
+                       + generatedColumns
+                       + "v100 varchar(400) default '1234567890123456789012345678901234567890"
+                       + "1234567890123456789012345678901234567890123456789012345')"
+                       + " SPLIT INTO 1 TABLETS;");
+
+    String dbStreamId = TestHelper.getNewDbStreamId("yugabyte", "t1", true);
+    Configuration.Builder configBuilder = TestHelper.getConfigBuilder("public.t1", dbStreamId);
+    configBuilder.with(YugabyteDBConnectorConfig.TOMBSTONES_ON_DELETE, false);
+
+    YBClient ybClient = TestHelper.getYbClient("127.0.0.1:7100");
+    String tableId = TestHelper.getYbTable(ybClient, "t1").getTableId();
+    Compactor compactor = new Compactor(tableId);
+
+    start(YugabyteDBConnector.class, configBuilder.build());
+
+    Thread t = new Thread(compactor);
+    t.start();
+    
+    String insertFormat = "INSERT INTO t1(id, name) VALUES (%s, 'value for split table');";
+
+    YugabyteDBConnection ybConnection = TestHelper.create();
+    Connection conn = ybConnection.connection();
+    Statement st = conn.createStatement();
+    
+    int beginKey = 0;
+    int endKey = beginKey + batchSize;
+    for (int i = 0; i < iterations; ++i) {
+      LOGGER.info("Inserting generate_series({}, {})", beginKey, endKey - 1);
+      st.execute(String.format(insertFormat, "generate_series(" + beginKey + ", " + (endKey-1) + ")"));
+      beginKey = endKey;
+      endKey = beginKey + batchSize;
+    }
+
+    LOGGER.info("Executing update");
+    st.execute("UPDATE t1 SET v100='updated value of v100' WHERE id % 2 = 0;");
+
+    LOGGER.info("Executing delete");
+    st.execute("DELETE FROM t1 WHERE id % 2 = 0;");
+
+    TestHelper.waitFor(Duration.ofSeconds(15));
+
+    // Consume the records and verify that the records should have the relevant information.
+    List<SourceRecord> records = new ArrayList<>();
+    long startMillis = System.currentTimeMillis();
+    CompletableFuture.runAsync(() -> getRecords(records, recordsCount + recordsCount /* updates + deletes */, millisecondsToWait)).get();
+    LOGGER.info("time taken to consume {} records: {}", recordsCount + recordsCount, System.currentTimeMillis() - startMillis);
+    
+    int updates = 0;
+    int deletes = 0;
+    // Check for updates
+    for (int j = 0; j < records.size(); ++j) {
+        LOGGER.debug("Verifying record: {}", j);
+        SourceRecord record = records.get(j);
+        Struct value = (Struct) record.value();
+        String op = value.getString("op");
+        if (op.equals("u")) {
+            LOGGER.info("ID: {}, Op: u", value.getStruct("after").getStruct("id").getInt32("value"));
+            ++updates;
+            // before image will have data
+            // Assert the columns for before image
+            for (int i = 1; i <= columnCount; ++i) {
+                assertValueField(record, "before/v" + i + "/value",
+                "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "1234567890123456789012345678901234567890123456789012345");
+
+                assertValueField(record, "after/v" + i + "/value",
+                "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "1234567890123456789012345678901234567890123456789012345");
+            }
+            assertValueField(record, "before/v100/value", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345");
+            
+            assertValueField(record, "after/v100/value", "updated value of v100");
+        } else if (op.equals("d")) {
+            LOGGER.info("ID: {}, Op: d", value.getStruct("before").getStruct("id").getInt32("value"));
+            ++deletes;
+            assertValueField(record, "after", null);
+            
+            // before image will have some data
+            // Assert the columns for before image
+            for (int i = 1; i <= columnCount; ++i) {
+                assertValueField(record, "before/v" + i + "/value",
+                "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "123456789012345678901234567890123456789012345678901234567890"
+                + "1234567890123456789012345678901234567890123456789012345");
+            }
+
+            assertValueField(record, "before/v100/value", "updated value of v100");
+        }
+    }
+
+    LOGGER.info("Total updates: {} and total deletes: {}", updates, deletes);
+
+    compactor.kill();
+  }
+
+  private class Compactor implements Runnable {
+    private String tableId;
+    private volatile boolean running = true;
+
+    public Compactor(String tableId) {
+        this.tableId = tableId;
+    }
+
+    @Override
+    public void run() {
+      LOGGER.info("Starting compactor thread");
+      try {
+        // Run compact table
+        YBClient ybClient = TestHelper.getYbClient("127.0.0.1:7100");
+
+        while (true) {
+            if (!running) {
+                break;
+            }
+
+            LOGGER.info("Compacting the table");
+            ybClient.flushTable(this.tableId);
+
+            Awaitility.await().pollDelay(Duration.ofSeconds(10)).atMost(Duration.ofSeconds(11))
+                .until(() -> {return true;});
+        }
+      } catch (Exception e) {
+        LOGGER.error("Exception while getting client or flushing table", e);
+        LOGGER.error("Shutting down compactor thread");
+      }
+    }
+
+    public void kill() {
+        running = false;
+    }
+  }
+
   private void assertBeforeImage(SourceRecord record, Integer id, String firstName, String lastName,
                                  Double hours) {
       assertValueField(record, "before/id/value", id);
@@ -336,7 +504,7 @@ public class YugabyteDBBeforeImageTest extends YugabyteDBTestBase {
                   return totalConsumedRecords.get() == totalRecordsToConsume;
               });
       } catch (ConditionTimeoutException exception) {
-          fail("Failed to consume " + totalRecordsToConsume + " records in " + seconds + " seconds",
+          fail("Failed to consume " + totalRecordsToConsume + " records in " + seconds + " seconds, total: " + totalConsumedRecords.get(),
                exception);
       }
 
